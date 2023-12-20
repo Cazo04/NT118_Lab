832cae92c3415d383e5f4351fcd2b8ce5be9ea01
Cazo <Cazo@DESKTOP-3TO1BJC>
2023-12-20 09:40:02 +0700

    Lab06: Setup

---
+++ app/src/main/java/com/example/testapp/Lab06/Lab06_Main.java
@@ -0,0 +1,16 @@
+package com.example.testapp.Lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.testapp.R;
+
+public class Lab06_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab06_main);
+    }
+}
